57e9b7fe948ffd9117f1c2652688e7bfc566881d
Corrado <corrado.lanera@gmail.com>
2020-05-15 20:49:03 +0200

    add file for develop setup

--- R/DEVELOP-SETUP.R
+++ R/DEVELOP-SETUP.R
@@ -5,6 +5,9 @@ usethis::create_package("rdojo.bowling")
 usethis::use_build_ignore("DEVELOP-SETUP.R") # this file
 usethis::use_readme_rmd()
 
+usethis::use_git()
+usethis::use_github()
+
+
+
 renv::init() # for reproducibility and portability
-
-
