d54304e930504bb156d0a7bdfe1f2255d9d6c350
Corrado <corrado.lanera@gmail.com>
2020-05-16 17:42:47 +0200

    test input are integer-like

--- tests/testthat/test-turn.R
+++ tests/testthat/test-turn.R
@@ -1,3 +1,12 @@
 test_that("turn return an object of class turn on a couple of integers", {
+
+  # output class
   expect_is(turn(1, 1), "turn")
+
+  # input classes
+  expect_error(turn(1, "1")) # char
+  expect_error(turn("1", 1))
+  expect_error(turn(1.5, 1)) # not integer-like
+  expect_error(turn(1, 1.5))
+
 })
