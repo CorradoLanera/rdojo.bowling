33403349410c2cef2fab54f52df063b885a54c27
Corrado <corrado.lanera@gmail.com>
2020-05-16 17:36:41 +0200

    define function turn to return a turn object

---
+++ R/turn.R
@@ -0,0 +1,13 @@
+#' Turn
+#'
+#' @param x
+#' @param y
+#'
+#' @return an object of class [turn]
+#' @export
+#'
+#' @examples
+#' turn(1, 1)
+turn <- function(x, y) {
+  structure(list(), class = "turn")
+}
