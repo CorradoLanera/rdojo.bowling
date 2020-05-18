86088e460b3685ad7ccf751da94d8c1292e679c7
Corrado <corrado.lanera@gmail.com>
2020-05-18 14:36:43 +0200

    test input object are of class turn

--- tests/testthat/test-game.R
+++ tests/testthat/test-game.R
@@ -1,5 +1,10 @@
+t1 <- turn(1, 2)
+
 test_that("game return an object of class game", {
-  t1 <- turn(1, 2)
   expect_is(game(t1), "game")
   expect_is(game(t1, t1), "game")
+})
+
+test_that("game trow error if not all objects are of class turn", {
+  expect_error(game(t1, 3))
 })
