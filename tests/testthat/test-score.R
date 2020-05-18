a7416a1e8c2bda52e721a4c87d28df6bc762968c
Corrado <corrado.lanera@gmail.com>
2020-05-18 23:30:07 +0200

    test for complex cases

--- tests/testthat/test-score.R
+++ tests/testthat/test-score.R
@@ -7,3 +7,22 @@ test_that("Compute real score for a turn", {
   expect_equal(eval_score_turn(g1, 2), 13)
   expect_equal(eval_score_turn(g1, 4), 17)
 })
+
+
+test_that("Compute real score for complex turns", {
+  g1 <- suppressWarnings(
+    game(turn(10), turn(10), turn(10), turn(10))
+  )
+  expect_equal(eval_score_turn(g1, 1), 30)
+  expect_equal(eval_score_turn(g1, 2), 30)
+
+  g2 <- suppressWarnings(
+    game(turn(5, 5), turn(10), turn(10), turn(10))
+  )
+  expect_equal(eval_score_turn(g2, 1), 20)
+
+  g3 <- suppressWarnings(
+    game(turn(10), turn(10), turn(5, 3))
+  )
+  expect_equal(eval_score_turn(g3, 1), 25)
+})
