7f6cb29ea6cbf71b8142f50f764ce1847d493f3d
Corrado <corrado.lanera@gmail.com>
2020-05-18 16:11:58 +0200

    test for game to be valid

--- tests/testthat/test-game.R
+++ tests/testthat/test-game.R
@@ -1,4 +1,6 @@
 t1 <- turn(1, 2)
+t_spare <- turn(3, 7)
+t_strike <- turn(10)
 
 test_that("game return an object of class game", {
   expect_is(game(t1), "game")
@@ -8,4 +10,58 @@ test_that("game return an object of class game", {
 test_that("game trow error if not all objects are of class turn", {
   expect_error(game(t1, 3), "3 is of class numeric")
   expect_error(game(t1, "foo", 3), '"foo" is of class character')
+})
+
+
+test_that("Do not collect more turns than possible", {
+  expect_error(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t1,
+         t1 # eleventh without a final spare
+    )
+  )
+  expect_error(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_spare,
+         t1, t1 # twelveth without two final consecutives strikes
+    )
+  )
+  expect_error(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_spare,
+         t_sprare, t1 # twelveth without two final consecutives strikes
+    )
+  )
+  expect_error(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_spare,
+         t_strike, t1 # twelveth without two final consecutives strikes
+    )
+  )
+  expect_error(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_strike,
+         t_1, t1 # twelveth without two final consecutives strikes
+    )
+  )
+  expect_error(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_strike,
+         t_sprare, t1 # twelveth without two final consecutives strikes
+    )
+  )
+})
+
+
+test_that("Accept the maximum number of turns required", {
+  expect_is(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t1),
+    "game"
+  )
+  expect_is(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_spare, t1),
+    "game"
+  )
+  expect_is(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_strike, t1),
+    "game"
+  )
+  expect_is(
+    game(t1, t1, t1, t1, t1, t1, t1, t1, t1, t_strike, t_strike, t1),
+    "game"
+  )
 })
