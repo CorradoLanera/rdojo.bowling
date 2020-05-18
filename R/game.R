20a96d6ea6a90abbb738ffe3e5a883e725d31746
Corrado <corrado.lanera@gmail.com>
2020-05-18 14:35:15 +0200

    games can take arbitrary inputs

--- R/game.R
+++ R/game.R
@@ -1,3 +1,18 @@
-game <- function(x) {
+#' Create game object
+#'
+#' A game is a sequence of [turn]s.
+#'
+#' @note The order of the turns matter!
+#'
+#' @param ... turns to include in the game
+#'
+#' @return an object of class [game]
+#' @export
+#'
+#' @examples
+#' t1 <- turn(1, 2)
+#' game(t1)
+#' game(t1, t1)
+game <- function(...) {
   structure(list(), class = "game")
 }
